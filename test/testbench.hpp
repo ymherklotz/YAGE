a4b4047e48e435253efea0c188b808995a477d8c
Yann Herklotz <ymherklotz@gmail.com>
2017-06-22 17:56:01 +0100

    Fixing file names

---
+++ test/testbench.hpp
@@ -0,0 +1,18 @@
+/*
+ * created 22-06-17 by Yann Herklotz
+ *
+ * Test bench to test different functions in the yage library
+ *
+ */
+
+#ifndef TEST_BENCH_HPP
+#define TEST_BENCH_HPP
+
+class TestBench
+{
+private:
+public:
+	
+};
+
+#endif
