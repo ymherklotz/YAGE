3cdb655e0e428c9c2891bea068e37543ae74fee9
Yann Herklotz <ymherklotz@gmail.com>
2017-07-28 00:24:38 +0100

    Updating docs

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['test',['Test',['../structTest.html',1,'']]],
-  ['testbench',['TestBench',['../classTestBench.html',1,'']]],
   ['texture',['Texture',['../structyage_1_1Texture.html',1,'yage']]],
   ['texturecache',['TextureCache',['../classyage_1_1TextureCache.html',1,'yage']]]
 ];
