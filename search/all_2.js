e7770985a2be00cc22c3cd1302afb1fcc16bf94b
Yann Herklotz <ymherklotz@gmail.com>
2017-07-28 00:38:30 +0100

    Updating docs

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['detail',['detail',['../namespacedetail.html',1,'']]],
   ['documentation',['Documentation',['../md_docs_README.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -2,23 +2,26 @@ var indexSectionsWithContent =
 {
   0: "bcdgimprstuvwy",
   1: "bcgimprstuvw",
-  2: "m",
-  3: "dy"
+  2: "d",
+  3: "m",
+  4: "dy"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "files",
-  3: "pages"
+  2: "namespaces",
+  3: "files",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Files",
-  3: "Pages"
+  2: "Namespaces",
+  3: "Files",
+  4: "Pages"
 };
 
