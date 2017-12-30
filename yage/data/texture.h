943e3a5bc98ebcc2aa1b1d576700f7c4010c143c
Yann Herklotz <ymherklotz@gmail.com>
2017-12-30 19:57:11 +0000

    [Code] Adding support for multiple textures.

--- yage/data/texture.h
+++ yage/data/texture.h
@@ -18,11 +18,13 @@ struct Texture {
     GLuint id;
     int width;
     int height;
+    int x;
+    int y;
 
-    Texture() : id(0), width(0), height(0) {}
+    Texture() : id(0), width(0), height(0), x(0), y(0) {}
 
-    Texture(GLuint id_i, int width_i, int height_i)
-        : id(id_i), width(width_i), height(height_i)
+    Texture(GLuint id_i, int width_i, int height_i, int x_i = 1, int y_i = 1)
+        : id(id_i), width(width_i), height(height_i), x(x_i), y(y_i)
     {
     }
 };
