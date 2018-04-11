ccdf76463228751dee100b544d854b6f1be5596c
Yann Herklotz <ymherklotz@gmail.com>
2018-04-11 22:24:46 +0100

    Adding 3D support

--- yage/experimental/camera3d.h
+++ yage/experimental/camera3d.h
@@ -1,8 +1,15 @@
 #ifndef YAGE_EXPERIMENTAL_CAMERA3D_H
 #define YAGE_EXPERIMENTAL_CAMERA3D_H
 
-class Camera3D {
-    
-}
+#include <glm/glm.hpp>
+
+class Camera3D
+{
+public:
+    Camera3D() = default;
+
+private:
+    glm::mat4 
+};
 
 #endif
---
+++ yage/experimental/loader.cpp
@@ -0,0 +1,45 @@
+#include "loader.h"
+
+#include "../core/exception.h"
+
+#include <fstream>
+#include <sstream>
+
+namespace yage
+{
+
+void load_obj(std::string filename, std::vector<glm::vec4> &vertices,
+              std::vector<glm::vec3> &normals, std::vector<GLushort> &elements)
+{
+    std::ifstream in(filename, std::ios::in);
+
+    if (!in.is_open()) {
+        throw FileLoadException("Could not load obj file '" + filename + "'");
+    }
+
+    std::string line;
+
+    while (getline(in, line)) {
+        if (line.substr(0, 2) == "v ") {
+            std::istringstream s(line.substr(2));
+            glm::vec4 v;
+            s >> v.x >> v.y >> v.z;
+            v.w = 1.f;
+            vertices.push_back(v);
+        } else if (line.substr(0, 2) == "f ") {
+            std::istringstream s(line.substr(2));
+            GLushort a, b, c;
+            s >> a >> b >> c;
+            a--, b--, c--;
+            elements.push_back(a);
+            elements.push_back(b);
+            elements.push_back(c);
+        } else {
+            // do nothing otherwise
+        }
+    }
+
+    normals.resize()
+}
+
+} // namespace yage
