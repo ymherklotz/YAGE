98b8df435329df6ff3a19ad46f13c8f5bb22ca53
Yann Herklotz <ymherklotz@gmail.com>
2018-06-15 20:00:39 +0100

    Adding entity system

--- yage/entity/entity.h
+++ yage/entity/entity.h
@@ -1,127 +1,50 @@
-/** ---------------------------------------------------------------------------
- * -*- c++ -*-
- * @file: entity.h
- *
- * Copyright (c) 2017 Yann Herklotz Grave <ymherklotz@gmail.com>
- * MIT License, see LICENSE file for more details.
- * ----------------------------------------------------------------------------
- */
-
-#pragma once
-
-#include <memory>
+#include <bitset>
 #include <vector>
 
-namespace yage
-{
-
-class Space;
-class ComponentGroup;
-
+/** 
+ * The entity is currently just an unsigned integer, which may change to a
+ * class in the future.
+ */
 typedef unsigned int Entity;
 
-/** Manages entities in a space.
+/** 
+ * The component mask represents all the components that the entity is
+ * currently attached to.
+ */
+typedef std::bitset<64> ComponentMask;
+
+class EntityManager;
+
+class BaseSystem;
+
+template <typename T>
+class System;
+
+class BaseComponent;
+
+template <typename T>
+class Component;
+
+class BaseComponentGroup;
+
+template <typename T>
+class ComponentGroup;
+
+/** 
+ * Has to keep track of all the different entities and their current state.
+ * 
+ * The key actions on an Entity are: deleting, creating.
  */
 class EntityManager
 {
 public:
-    /** Default instance of an EntityManager.
-     */
-    EntityManager() = default;
-
-    /** Creates an instance of the entity manager, which refers back to the
-     * space it was created in and belongs to.
-     *
-     * @param space Current space that the EntityManager belongs to.
-     */
-    EntityManager(Space *space);
-
-    /** Creates an instance of the entitiy manager with an initial size.
-     *
-     * @param space Current space that the EntityManager belongs to.  @param n
-     * Initial size of the EntityManager.
-     */
-    EntityManager(Space *space, std::size_t n);
-
-    /** Creates an Entity and returns the handle to the entity, which can then
-     * be used by the user to do operations on it.
-     *
-     * @return The handle to the entity that was created in the space.
-     */
     Entity createEntity();
-
-    /** Delete an entity.
-     */
-    void deleteEntity(Entity entity);
+    EntityManager &deleteEntity(Entity entity);
+    void addComponent(Entity entity, BaseComponent *component);
 
 private:
-    /** The next available handle to give to the user.
-     */
-    Entity next_entity_ = 0;
+    Entity next_entity_;
 
-    /** The space that the entity manager belongs to.
-     */
-    Space *space_ = nullptr;
-
-    /** The entities in the current space.
-     */
-    std::vector<Entity> entities_;
-
-    /** Component list of different component groups, that then contain the
-     * different components for the entities.
-     */
-    std::vector<std::unique_ptr<ComponentGroup>> components;
+    std::vector<BaseComponentGroup *> component_group_;
+    std::vector<ComponentMask> component_masks_;
 };
-
-/** Base component used to store components.
- *
- * Should not be inherited from when declaring a new component. Instead, the
- * Component<Derived> should be used.
- */
-class BaseComponent
-{
-public:
-    /** Group used to register a specific component internally with the entity
-     * manager.
-     */
-    typedef unsigned Group;
-
-protected:
-    static Group group_id_counter_;
-};
-
-/** The main component that is used to make a component from a defined struct.
- *
- * A component should only be declared as a struct and should not contain any
- * data itself.
- */
-template <typename Derived>
-class Component : public BaseComponent
-{
-private:
-    friend class EntityManager;
-
-    BaseComponent::Group group();
-};
-
-/** Contains a list of all components that belong to a sepecific group, these
- * are then stored in the main entity manager.
- */
-class ComponentGroup
-{
-private:
-    friend class EntityManager;
-
-    std::vector<BaseComponent *> components_;
-};
-
-// Template definitions
-
-template <typename Derived>
-BaseComponent::Group Component<Derived>::group()
-{
-    static Group group_id = group_id_counter_++;
-    return group_id;
-}
-
-} // namespace yage
