65c75747db540be33dab64b23059de1387daf220
Yann Herklotz <ymherklotz@gmail.com>
2018-06-15 20:00:46 +0100

    Adding new system

--- yage/entity/system.h
+++ yage/entity/system.h
@@ -1,26 +1,10 @@
-/** ---------------------------------------------------------------------------
- * -*- c++ -*-
- * @file: system.h
- *
- * Copyright (c) 2017 Yann Herklotz Grave <ymherklotz@gmail.com>
- * MIT License, see LICENSE file for more details.
- * ----------------------------------------------------------------------------
- */
-
-#pragma once
-
-#include <cstddef>
-
-#include "../util/noncopyable.h"
-
-namespace yage
-{
-
-class System : public NonCopyable
+class BaseSystem
 {
 public:
-    virtual void init()   = 0;
     virtual void update() = 0;
 };
 
-} // namespace yage
+template <typename T>
+class System : public BaseSystem
+{
+};
