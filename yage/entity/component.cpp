3702e753a5f7b31c31261c968757e19e808a84ec
Yann Herklotz <ymherklotz@gmail.com>
2018-06-22 00:08:12 +0100

    Adding components implementation

--- yage/entity/component.cpp
+++ yage/entity/component.cpp
@@ -1,6 +1,8 @@
 #include "component.h"
 
-GroupId BaseComponent::getGroup() {
-    static GroupId group_id = group_id_counter_++;
-    return group_id;
-}
+namespace yage
+{
+
+GroupId BaseComponent::group_id_counter_ = 0;
+
+} // namespace yage
