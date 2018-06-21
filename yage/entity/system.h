a88e5ca9a0db09a1e46fedfe7d7e03918578e911
Yann Herklotz <ymherklotz@gmail.com>
2018-06-22 00:07:46 +0100

    Writing systems for entity system

--- yage/entity/system.h
+++ yage/entity/system.h
@@ -1,10 +1,21 @@
+#pragma once
+
+#include "entity.h"
+
+namespace yage
+{
+
 class BaseSystem
 {
 public:
-    virtual void update() = 0;
+    virtual void update(double dt, EntityManager &em) = 0;
 };
 
 template <typename T>
 class System : public BaseSystem
 {
+protected:
+    ComponentMask active_mask_;
 };
+
+} // namespace yage
